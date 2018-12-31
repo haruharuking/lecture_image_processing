03a2365ceacb373bf81507df1e9e09f65aa9887f
haruki machida <16ec112@ms.dendai.ac.jp>
2018-12-31 22:32:43 +0900

    プリグラムの改変

--- kadai2.m
+++ kadai2.m
@@ -1,25 +1,30 @@
 % ‰Û‘è‚Q@ŠK’²”‚Æ‹^Ž——ÖŠs
 % ‚QŠK’²C‚SŠK’²C‚WŠK’²‚Ì‰æ‘œ‚ð¶¬‚¹‚æD
 % ‰º‹L‚ÍƒTƒ“ƒvƒ‹ƒvƒƒOƒ‰ƒ€‚Å‚ ‚éD
-% ‰Û‘èì¬‚É‚ ‚½‚Á‚Ä‚ÍuLennavˆÈŠO‚Ì‰æ‘œ‚ð—p‚¢‚æD
+% ‰Û‘èì¬‚É‚ ‚½‚Á‚Ä‚ÍuLennavˆÈŠO‚Ì‰æ‘œ‚ð—p‚¢‚æ
 
-clear; % •Ï”‚ÌƒI[ƒ‹ƒNƒŠƒA
+ORG = imread('outdoor_101_rika.jpg');
+ORG = rgb2gray(ORG);
+imagesc(ORG)
 
-ORG=imread('Lenna.png'); % Œ´‰æ‘œ‚Ì“ü—Í
-ORG = rgb2gray(ORG); colormap(gray); colorbar;
-imagesc(ORG); axis image; % ‰æ‘œ‚Ì•\Ž¦
-pause; % ˆêŽž’âŽ~
+% 2ŠK’²‰æ‘œ‚Ì¶¬
+IMG = ORG>128; 
+imagesc(IMG)
 
-% ‚QŠK’²‰æ‘œ‚Ì¶¬
-IMG = ORG>128;
-imagesc(IMG); colormap(gray); colorbar;  axis image;
-pause;
 
-% ‚SŠK’²‰æ‘œ‚Ì¶¬
+% 4ŠK’²‰æ‘œ‚Ì¶¬
 IMG0 = ORG>64;
 IMG1 = ORG>128;
 IMG2 = ORG>192;
 IMG = IMG0 + IMG1 + IMG2;
-imagesc(IMG); colormap(gray); colorbar;  axis image;
+imagesc(IMG)
 
-% ‚WŠK’²‚É‚Â‚¢‚Ä‚ÍCŠeŽ©ŒŸ“¢‚µ‚Ä‚­‚¾‚³‚¢D
+% 8ŠK’²‰æ‘œ‚Ì¶¬
+IMG0 = ORG>32;
+IMG1 = ORG>64;
+IMG2 = ORG>128;
+IMG3 = ORG>160;
+IMG4 = ORG>192;
+IMG5 = ORG>224;
+IMG6 = ORG>256;
+IMG = IMG0 + IMG1 + IMG2 +IMG3 +IMG4 +IMG5 +IMG6;
